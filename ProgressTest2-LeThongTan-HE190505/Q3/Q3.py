7243769d57bf4c1dc49f0b24122d7475843abfa4
LukeSaccius <154858320+LukeSaccius@users.noreply.github.com>
2024-03-21 17:22:42 +0700

    1

--- ProgressTest2-LeThongTan-HE190505/Q3/Q3.py
+++ ProgressTest2-LeThongTan-HE190505/Q3/Q3.py
@@ -1,52 +1,75 @@
-# Define the dynamic programming approach to solve the knapsack problem as given in the example.
-def knapsack_dp(values, weights, max_weight):
-    n = len(values)
-    # Initialize the DP table
-    dp = [[0 for j in range(max_weight + 1)] for i in range(n + 1)]
-    for i in range(1, n + 1):
-        for j in range(1, max_weight + 1):
-            if weights[i-1] > j:
-                dp[i][j] = dp[i-1][j]
+# Since we are to read from a file, let's assume that the content of BAG.INP is provided as a string for this environment.
+# We will then simulate file reading and writing process.
+
+bag_inp_content = """5 11
+3 3
+4 4
+5 4
+9 10
+4 4"""
+
+# Writing the BAG.INP content to a file
+with open('BAG.INP', 'w') as file:
+    file.write(bag_inp_content)
+
+# Now we define the function to read the input file and solve the knapsack problem
+def read_from_file(file_path):
+    with open(file_path, 'r') as file:
+        N, W = map(int, file.readline().split())
+        items = [tuple(map(int, line.split())) for line in file]
+    return N, W, items
+
+# Define the knapsack solver function
+def knapsack_solver(N, W, items):
+    # Create a DP table to store the maximum value that can be attained with given weight
+    dp = [[0 for x in range(W + 1)] for x in range(N + 1)]
+    
+    # Build table in bottom up manner
+    for i in range(N + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                dp[i][w] = 0
+            elif items[i-1][0] <= w:
+                dp[i][w] = max(items[i-1][1] + dp[i-1][w-items[i-1][0]], dp[i-1][w])
             else:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weights[i-1]] + values[i-1])
-    return dp[n][max_weight], dp
+                dp[i][w] = dp[i-1][w]
 
-# Define the function to determine the items included in the knapsack
-def determine_items(dp, weights, values, max_weight):
-    n = len(values)
-    i, w = n, max_weight
-    items = []
-    
-    while i > 0 and w >= 0:
-        if dp[i][w] != dp[i-1][w]:
-            items.append(i)
-            w -= weights[i-1]
-            i -= 1
+    # Store the result of the knapsack
+    result = dp[N][W]
+
+    # Determine which items to take
+    w = W
+    taken_items = []
+    for i in range(N, 0, -1):
+        if result <= 0:
+            break
+        if result == dp[i-1][w]:
+            continue
         else:
-            i -= 1
-    
-    items.reverse()
-    return items
+            taken_items.append(i)
+            result -= items[i-1][1]
+            w -= items[i-1][0]
 
-# Read the input file BAG.INP and extract the values and weights
-with open('BAG.INP', 'r') as f:
-    n, W = map(int, f.readline().split())
-    values = []
-    weights = []
-    for _ in range(n):
-        weight, value = map(int, f.readline().split())
-        weights.append(weight)
-        values.append(value)
+    return dp[N][W], taken_items[::-1]
 
-# Compute the knapsack solution
-max_value_dp, dp_table = knapsack_dp(values, weights, W)
-items = determine_items(dp_table, weights, values, W)
+# Define the function to write the solution to the output file
+def write_to_file(file_path, max_value, taken_items):
+    with open(file_path, 'w') as file:
+        file.write(f"{max_value}\n")
+        for item in taken_items:
+            file.write(f"{item}\n")
 
-# Generate the output string for BAG.OUT
-output_str = f"{max_value_dp}\n" + " ".join(str(item) for item in items) + " 1"
+# Read the problem from the file
+N, W, items = read_from_file('BAG.INP')
 
-# Write the output to BAG.OUT file
-output_file_path = 'BAG.OUT'
-with open(output_file_path, 'w') as f:
-    f.write(output_str)
+# Solve the knapsack problem
+max_value, taken_items = knapsack_solver(N, W, items)
 
+# Write the solution to the output file
+write_to_file('BAG.OUT', max_value, taken_items)
+
+# The BAG.OUT content for verification purposes
+with open('BAG.OUT', 'r') as file:
+    output_content = file.read()
+
+output_content
